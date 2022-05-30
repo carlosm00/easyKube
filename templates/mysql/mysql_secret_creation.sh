73a5425c263d1e41ecb5e46966309f8737db309f
Carlos Mena <menacarlos.asir@gmail.com>
2022-05-31 01:15:33 +0200

    corrected secrets

--- templates/mysql/mysql_secret_creation.sh
+++ templates/mysql/mysql_secret_creation.sh
@@ -1,5 +1,5 @@
-kubectl create secret generic mysql-root-pass --from-literal=password=YUIidhb667
+kubectl create secret generic mysql-root-pass --from-literal=password=<add password>
 
-kubectl create secret generic mysql-user-pass --from-literal=username=kodekloud_rin --from-literal=password=BruCStnMT5
+kubectl create secret generic mysql-user-pass --from-literal=username=<add username> --from-literal=password=<add password>
 
-kubectl create secret generic mysql-db-url --from-literal=database=kodekloud_db10
+kubectl create secret generic mysql-db-url --from-literal=database=<add database>
