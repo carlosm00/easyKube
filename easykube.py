0f1bcf119a0470c4c3e9e3f091aa61c103b26b81
Carlos Mena <menacarlos.asir@gmail.com>
2022-06-02 00:58:57 +0200

    Added travis file

--- easykube.py
+++ easykube.py
@@ -9,8 +9,46 @@ YAML templates are stored on the templates folder.
 Library importation
 """
 
+from __future__ import print_function
 import sys
 import logging
 import os
+import json
+import yaml
+import time
 from os import listdir
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 
+# Default namespace will be easykube
+NAMESPACE = "easykube"
+
+
+# Action Function list
+## List Pods from namespace
+def list_pods():
+    print("Listing pods with their IPs:")
+    request = v1.list_namespaced_pod(NAMESPACE)
+    for i in request.items:
+        print("%s\t%s\t%s\t%s\t%s" % (i.metadata.name, i.status.pod_ip,
+                              i.metadata.namespace, i.metadata.name, i.status.phase))
+
+## List Deployments
+def list_deployments():
+    print("Listing deployments:")
+    request = v1.list_namespaced_deployment(NAMESPACE, pretty=True, timeout_seconds=3, watch=False)
+    print(request)
+
+## 
+
+# Main function
+def main():
+    # Loading K8s config
+    config.load_kube_config()
+
+    # Setting client
+    v1 = client.CoreV1Api()
+
+
+if __name__ == "__main__":
+    main()
